3e57aea50cff26371e1dc9526bc49ffd3d32168e
Hank Lee <hank7468@gmail.com>
2019-08-13 11:40:12 +0800

    add new file

---
+++ ftoctransfer.py
@@ -0,0 +1,5 @@
+# Fahrenheit to Celsius transaction
+celsius = input('please input celsius temperature: ')
+fahrenheit = float(celsius) * 9 / 5 + 32
+# +-*/算符與數字間有空格較佳
+print('fahrenheit is as following: ', fahrenheit)
